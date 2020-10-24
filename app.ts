127d034ff4cb6bee3ac0396314327bfc9844371f
villasurlamer <loic.dekester@gmail.com>
2020-10-23 21:30:48 -0700

    update http error handler

--- app.ts
+++ app.ts
@@ -5,7 +5,6 @@ import logger from 'morgan';
 import indexRouter from './src/routes/index';
 import HttpException from './src/exceptions/HttpException'
 
-
 const app = express();
 
 app.use(logger('dev'));
@@ -15,21 +14,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 /** Routes */
 app.use('/', indexRouter);
+
 /** catch 404 and forward to error handler */
 app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
   const err: HttpException = new HttpException(404, `Not Found: ${req.ip} tried to reach ${req.originalUrl}`);
   next(err);
 });
+
 /** Error Handler */
-app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-  console.log(err.stack);
-  res.status(err.status || 500);
-  res.json({
-    'errors': {
-      message: err.message,
-      error: err
-    }
-  });
+app.use(function (err: HttpException, req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.status(err.status).send({ error: err.message });
 });
 
 export default app;
--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -1,7 +1,7 @@
 class HttpException extends Error {
   status: number;
   message: string;
-  constructor(status: number, message: string) {
+  constructor(status: number = 500, message: string = 'Something went wrong') {
     super(message);
     this.status = status;
     this.message = message;
