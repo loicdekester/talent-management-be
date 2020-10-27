a0e37460485991731a77e8e54dc8e7ebb547f9ab
villasurlamer <loic.dekester@gmail.com>
2020-10-26 19:06:05 -0700

    add cors headers

--- app.ts
+++ app.ts
@@ -12,6 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', `${process.env.HOST}:${process.env.PORT}`);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 /** Routes */
 app.use('/', indexRouter);
 
