e243432f662720c6dce746d03cde13080dd3e687
villasurlamer <loic.dekester@gmail.com>
2020-10-29 17:21:09 -0700

    deletion of languages

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import users from './users';
 import experiences from './experiences';
 import educations from './educations';
+import languages from './languages';
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/experiences', experiences);
 router.use('/educations', educations);
+router.use('/languages', languages);
 
 router.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
   return next(err);
